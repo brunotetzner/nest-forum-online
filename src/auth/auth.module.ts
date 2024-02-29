c6396b41ea7ef68a3c7a7770d1c0f2a795683212
Bruno Tetzner <brunoltetzner@gmail.com>
2024-02-29 20:38:23 -0300

    feat: implements route to authenticate user

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { Env } from "src/env";
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      global: true,
+      useFactory(config: ConfigService<Env, true>) {
+        const privateKey = config.get("JWT_PRIVATE_KEY", { infer: true });
+        const publicKey = config.get("JWT_PUBLIC_KEY", { infer: true });
+        console.log("key 1", privateKey);
+        console.log("key 2", publicKey);
+        return {
+          signOptions: {
+            algorithm: "RS256",
+          },
+          privateKey: Buffer.from(privateKey, "base64"),
+          publicKey: Buffer.from(publicKey, "base64"),
+        };
+      },
+    }),
+  ],
+})
+export class AuthModule {}
