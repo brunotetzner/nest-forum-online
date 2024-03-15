ab76f81d88e4f032452ea36170c3e4b49f2f0940
Bruno Tetzner <brunoltetzner@gmail.com>
2024-03-15 19:26:22 -0300

    feat: create questions workflow working

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -2,11 +2,11 @@ import { PaginationParams } from "@/core/repositories/pagination-params";
 import { Question } from "../../enterprise/entities/question";
 import { Slug } from "../../enterprise/entities/value-objec/slug";
 
-export interface QuestionsRepository {
-  findById(id: string): Promise<Question | null>;
-  create(answer: Question): Promise<void>;
-  save(answer: Question): Promise<void>;
-  findBySlug(slug: Slug): Promise<Question | null>;
-  delete(question: Question): Promise<void>;
-  findManyRecent(params: PaginationParams): Promise<Question[]>;
+export abstract class QuestionsRepository {
+  abstract findById(id: string): Promise<Question | null>;
+  abstract create(answer: Question): Promise<void>;
+  abstract save(answer: Question): Promise<void>;
+  abstract findBySlug(slug: Slug): Promise<Question | null>;
+  abstract delete(question: Question): Promise<void>;
+  abstract findManyRecent(params: PaginationParams): Promise<Question[]>;
 }
