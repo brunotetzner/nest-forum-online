a7c176115f9479028bd1315d0a88838aa84c89eb
Bruno Tetzner <brunoltetzner@gmail.com>
2024-03-20 07:38:29 -0300

    feat: list questions

--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -3,7 +3,6 @@ import { CurrentUser } from "@/infra/auth/current-user-decorator";
 import { JwtAuthGuard } from "@/infra/auth/jwt-auth-guard";
 import { userPayload } from "@/infra/auth/jwt.strategy";
 import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
-import { PrismaService } from "@/infra/database/prisma/prisma.service";
 import { z } from "zod";
 import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
 
